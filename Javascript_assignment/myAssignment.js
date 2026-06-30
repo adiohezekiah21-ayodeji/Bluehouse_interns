d8f91b80653b7350a60a16dd95b9e2e6dc4dd0c0
Hezekiah <adiohezekiah21@gmail.com>
2026-06-30 09:23:46 +0100

    Javascript assignment

---
+++ Javascript_assignment/myAssignment.js
@@ -0,0 +1,41 @@
+/*Five different examples of each of the 
+three Javascript variables types*/
+
+//First variable type is: var
+//Examples of var
+
+var firstName="Hezekiah"; //stores my name
+var School="University of Jos"; // Name of school
+var isStudent=true; //stores whether someone is a student or not
+var salary=50000; //stores an employee's salary.
+
+
+//Second variable type is: let
+//Examples of let
+
+let city="Jos"; //stores the name of a city
+let score="95"; // stores a student's score
+let temperature=28; //stores the current temperature
+let course="Computer Science"; //stores course of study
+let accountBalance=12000000000; //bank account balance;
+
+
+//Third variable type is: const
+//Examples of const
+
+const pi=3.142; //stores mathematical value of pi
+const daysInWeek=7; //stores the number of days in a week
+const passMark=40; //stores the minimum score required to pass.
+const currency="Naira"; //stores the name of country's currency
+
+//to display some values stored inside the container
+console.log(city);
+console.log(score);
+console.log(isStudent);
+console.log(accountBalance);
+console.log(pi);
+console.log(passMark);
+console.log(firstName);
+console.log(School);
+console.log(salary);
+
